e26ad7534a766d4e8eded3e6a6827268b25fe459
unduli-attacked <45951442+unduli-attacked@users.noreply.github.com>
2019-04-28 19:28:34 -0700

    yay api!

--- src/main/java/base/Main.java
+++ src/main/java/base/Main.java
@@ -3,10 +3,27 @@ package base;
 
 import java.util.ArrayList;
 
+import com.cpjd.main.TBA;
+
 public class Main{
+    public static TBA tbaApi;
     static ArrayList<Team> teams = new ArrayList<Team>();
+    static ArrayList<Match> matches = new ArrayList<Match>();
+
+    //TODO make some sort of session file to store these
+    static final int season = 2019;
+    static final String event = "Sacramento";
+    static final String tbaEventKey = "2019cada"; //FIXME i hope
+    static final String eventDir = "sac_2019/";
+    static final String dataDir = eventDir+"data/";
+    static final String sortsDir = eventDir+"sorts/";
+    static final String teamsDir = eventDir+"teams/";
 
     public static void main(String[] args){
-        System.out.println("Hello world");
+        TBA.setAuthToken("OPynqKt8K0vueAXqxZzdigY9OBYK3KMgQQrsM4l8jE5cBmGfByhy6YzVIb2Ts7xD");
+        tbaApi = new TBA();
+        System.out.println(tbaApi.getEventAwards(tbaEventKey)[0].getName());
     }
+
+
 }
--- src/main/java/base/Match.java
+++ src/main/java/base/Match.java
@@ -3,6 +3,11 @@ package base;
 
 public class Match{
 
+    public int teamNum;
+    public String matchType;
+    public int matchNum;
+    public String alliancePosition;
+
     public int HPShipGame;
     public int HPShipSand;
     public int HPRocketGame;
@@ -29,11 +34,50 @@ public class Match{
     public boolean eStopped;
     public boolean borked;
 
-    public Match(String[] csvRow){
+    public int scoutedPoints;
+    public int tbaPoints;
 
+    public String matchNotes;
+
+    public Match(String[] csvRow){
+        this.matchType = csvRow[0];
+        this.matchNum = Integer.valueOf(csvRow[1]);
+        this.teamNum = Integer.valueOf(csvRow[2]);
+        this.alliancePosition = csvRow[3];
+
+        this.startHab = Integer.valueOf(csvRow[4]);
+
+        this.CShipSand = Integer.valueOf(csvRow[5]);
+        this.CRocketSand = Integer.valueOf(csvRow[6]);
+        this.CDropSand = Integer.valueOf(csvRow[7]);
+        this.HPShipSand = Integer.valueOf(csvRow[8]);
+        this.HPRocketSand = Integer.valueOf(csvRow[9]);
+        this.HPDropSand = Integer.valueOf(csvRow[10]);
+
+        this.CShipGame = Integer.valueOf(csvRow[11]);
+        this.CRocketGame = Integer.valueOf(csvRow[12]);
+        this.CDropGame = Integer.valueOf(csvRow[13]);
+        this.HPShipGame = Integer.valueOf(csvRow[14]);
+        this.HPRocketGame = Integer.valueOf(csvRow[15]);
+        this.HPDropGame = Integer.valueOf(csvRow[16]);
+
+        this.scaleLevel = Integer.valueOf(csvRow[17]);
+
+        this.techs = Integer.valueOf(csvRow[18]);
+        this.fouls = Integer.valueOf(csvRow[19]);
+        this.yellow = (csvRow[20].equalsIgnoreCase("true"));
+        this.red = csvRow[21].equalsIgnoreCase("true");
+        this.borked = csvRow[22].equalsIgnoreCase("true");
+        this.scoutedPoints = Integer.valueOf(csvRow[23]);
+
+        this.matchNotes = csvRow[24];
     }
 
     public int getSandPlaces(){
         return HPShipSand+HPRocketSand+CShipSand+CRocketSand;
+    }
+
+    private void getTBAVals(int matchNum){
+
     }
 }
