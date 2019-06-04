13de7ae37d1368344cc767facf4dd2c1c05b616b
unduli-attacked <45951442+unduli-attacked@users.noreply.github.com>
2019-06-04 11:32:59 -0700

    javafx!

--- src/main/java/base/Main.java
+++ src/main/java/base/Main.java
@@ -12,14 +12,38 @@ import com.cpjd.main.TBA;
 import com.cpjd.models.events.Award;
 import com.cpjd.models.teams.Team;
 
-public class Main{
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
     public static TBA tbaApi;
     public static Session sesh = new Session(2019, "Sac 2019", "2019cada", "sac_2019/", new Color(100, 199, 254));
 
-    public static void main(String[] args) throws Exception{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
         TBA.setAuthToken("OPynqKt8K0vueAXqxZzdigY9OBYK3KMgQQrsM4l8jE5cBmGfByhy6YzVIb2Ts7xD");
         tbaApi = new TBA();
 
+        primaryStage.setTitle("Team 5940 Scouting Base");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 
 
